bf8478b04ab253a9f520930e66c5f7be8433316b
Kewei-cpu <84075255+Kewei-cpu@users.noreply.github.com>
2024-06-14 19:33:28 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full h-[100vh] bg-gray-900 flex flex-col justify-center items-center">
+    <h1 class="text-6xl font-bold text-purple-600">
+      HAJAHA <br>
+      Universe
+    </h1>
+  </div>
+
+</template>
+<script lang="ts" setup>
+</script>
