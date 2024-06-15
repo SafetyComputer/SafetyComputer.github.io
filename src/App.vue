ba642e1bde0d87107035b9a3f9c0e4be75bc0120
Kewei-cpu <84075255+Kewei-cpu@users.noreply.github.com>
2024-06-15 09:36:22 +0800

    add universe

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="w-full h-[100vh] bg-gray-900 flex flex-col justify-center items-center">
+  <div class="w-full h-[100vh] bg-gray-900 flex flex-col items-center">
     <h1 class="text-6xl font-bold text-purple-600">
       HAJAHA <br>
       Universe
     </h1>
+
+    <div class=" w-full h-full">
+      <div class="border-2 border-gray-100 rounded-[50%] h-full flex justify-center items-center">
+        <div class="border-2 border-gray-100 rounded-[50%] w-[80%] h-[80%] flex justify-center items-center">
+                  <div class="border-2 border-gray-100 rounded-[50%] w-[80%] h-[80%] flex justify-center items-center">
+        <div class="border-2 border-gray-100 rounded-[50%] w-[80%] h-[80%] flex justify-center items-center">
+        <div class="border-2 border-gray-100 rounded-[50%] w-[80%] h-[80%] flex justify-center items-center">
+        <div class="border-2 border-gray-100 rounded-[50%] w-[80%] h-[80%] flex justify-center items-center">
+          <div class="bg-yellow-100 rounded-[50%] w-20 h-20 "></div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+      </div>
+    </div>
   </div>
 
 </template>
-<script lang="ts" setup>
+<script setup>
 </script>
